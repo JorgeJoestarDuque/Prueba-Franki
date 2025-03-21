ab615a4f5dcbb4eae5213a0ca3e5d174048dc3ca
JorgeJoestarDuque <duquerestrepojorgeesteban@gmail.com>
2025-03-20 22:29:42 -0500

    Aplication of style

--- frontendPruebaFranki/src/components/TaskManager.jsx
+++ frontendPruebaFranki/src/components/TaskManager.jsx
@@ -1,44 +1,65 @@
-import { useState } from 'react'
+import { useState } from "react";
+import "./TaskManager.css";
 
-const TaskManager = ({onPostTask}) => {
+const TaskManager = ({ onPostTask }) => {
+  //Tarea sin crear
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
 
-    //Tarea sin crear
-    const [formData, setFormData] = useState({
-        title:"",
-        description:""
-    })
+  //Sobreescribe la data correspondiente
+  const handleOnChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    //Sobreescribe la data correspondiente
-    const handleOnChange = (event) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value
-        })
-    }
-
-    //Publica la data
-    const handleOnSubmit = (event) => {
-        event.preventDefault();
-        onPostTask(formData)
-    }
-    return(
-        <>
-            <div>
-                <h2>Gestor de tareas</h2>
-                <form onSubmit={handleOnSubmit}>
-                    <label>
-                        Titulo
-                        <input type="text" name="title" value={formData.title} onChange={handleOnChange}/>
-                    </label>
-                    <label>
-                        Descripción
-                        <textarea type="text" name="description" value={formData.description} onChange={handleOnChange}/>
-                    </label>
-                    <button type="submit">Crear</button>
-                </form>
-            </div>
-        </>
-    )
-}
+  //Publica la data
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    onPostTask(formData);
+    setFormData({ title: "", description: "" });
+  };
+  return (
+    <>
+      <div>
+        <form onSubmit={handleOnSubmit} className="form">
+          <h2 className="form_title">Gestor de tareas</h2>
+          <p className="text">Titulo</p>
+          <label className="form_label">
+            <input
+              className="form_input"
+              placeholder=""
+              type="text"
+              name="title"
+              value={formData.title}
+              onChange={handleOnChange}
+            />
+            <span className="form_text">Ingresa el titulo.</span>
+          </label>
+          <p className="text">Descripción</p>
+          <label className="form_label">
+            <textarea
+              className="form_input"
+              placeholder=""
+              type="text"
+              name="description"
+              value={formData.description}
+              onChange={handleOnChange}
+            />
+            <span className="form_text">
+              Ingresa la descripcion(Es opcional).
+            </span>
+          </label>
+          <button className="form_submit" type="submit">
+            Crear
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default TaskManager;
